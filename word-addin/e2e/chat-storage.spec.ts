a23287cbc00cd84934835d1fe4b849f89c10164b
willchen96 <weiye.chen96@icloud.com>
2026-08-23 21:48:05 +0800

    test(word-addin): update liquid glass assertions

--- word-addin/e2e/chat-storage.spec.ts
+++ word-addin/e2e/chat-storage.spec.ts
@@ -69,12 +69,12 @@ test("cloud is default and local mode persists document chats in IndexedDB", asy
     cloudSwitch.locator(
       "xpath=ancestor::div[contains(@class, 'rounded-xl')][1]",
     ),
-  ).toHaveClass(/bg-white\/55/);
+  ).toHaveClass(/liquid-glass-flat/);
   await expect(
     page
       .getByRole("button", { name: "Delete" })
       .locator("xpath=ancestor::div[contains(@class, 'rounded-xl')][1]"),
-  ).toHaveClass(/bg-white\/55/);
+  ).toHaveClass(/liquid-glass-flat/);
   await expect(cloudSwitch).toBeChecked();
   await cloudSwitch.click();
   await expect(cloudSwitch).not.toBeChecked();
